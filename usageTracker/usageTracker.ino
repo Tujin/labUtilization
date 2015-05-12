37f1e87157558cb874e27c29a35b5654e3eac8d3
Terry Ulery <Terry.L.Ulery@intel.com>
2015-05-11 17:03:39 -0700

    Updated arduino sketch to match changes to touchscreen program. Can highlight WWID or Duration field, pressing tool field cycles through available tools.

--- usageTracker/usageTracker.ino
+++ usageTracker/usageTracker.ino
@@ -1,14 +1,42 @@
 #include <genieArduino.h>
 
+// definitions for key strokes
 #define BACKSPACE 110
 #define CANCEL 107
 #define SUBMIT 13
+
+//definitions for buffer sizes
 #define WWID_SIZE 8
+#define DUR_SIZE 2
+
+//definitions for screen objects
+#define STRING_WWID 0
+#define STRING_DUR 8
+#define STRING_TOOL 9
+#define STRING_WWID_BUTTON_MASK 1
+#define STRING_DUR_BUTTON_MASK 2
+#define BUTTON_WWID 0
+#define BUTTON_DUR 1
+#define BUTTON_TOOL 2
+
+//Definitions for selected int
+#define SELECTED_NONE 0
+#define SELECTED_WWID 1
+#define SELECTED_DUR 2
+
+//Definitions for tool strings object
+#define TOOL_STRINGS_ELEMENTS 8
 
 Genie genie;
 
-char wwid[WWID_SIZE + 1]; // declare a char array to hold our WWID, add one for null terminator
+char wwidBuf[WWID_SIZE + 1]; // declare a char array to hold our WWID, add one for null terminator
+char durBuf[DUR_SIZE + 1]; // declare a char array to hold keystrokes for duration
+
 int wwidIndex;
+int durIndex;
+int selectedButton;
+int selectedTool;
+boolean selectedChange;
 
 void setup()
 {  
@@ -25,6 +53,12 @@ void setup()
   
   
   genie.WriteObject (GENIE_OBJ_FORM, 0, 0); // Switch to form 0
+  selectedButton = SELECTED_DUR; // start with WWID selected
+  selectedTool = 0; // start with first string selected in tool list
+  selectedChange = true;
+  wwidBuf[0] = 0; // set first character as null terminator
+  durBuf[0] = 0; // set first character as null terminator
+  drawScreen(); // draw screen for the first time
   genie.WriteObject(GENIE_OBJ_STRINGS,0,0); // display the box for strings object, without this line the box doesn't appear until you begin typing
   Serial.println("Setup finished.");
 }
@@ -34,7 +68,24 @@ void loop()
   genie.DoEvents(); // This calls the library each loop to process the queued responses from the display
 }
 
-void keyPress(int key)
+void drawScreen()
+{
+  genie.WriteObject(GENIE_OBJ_STRINGS,STRING_TOOL,selectedTool); // draw tool button with appropriate string selected
+  if(selectedChange)
+  {
+    genie.WriteObject(GENIE_OBJ_USERBUTTON,BUTTON_WWID,0); // bring black wwid user button to the front, obscuring the text element and highlighting box
+    genie.WriteObject(GENIE_OBJ_USERBUTTON,BUTTON_DUR,0); // bring black duration user button to the front, obscuring the text element and highlighting box
+    selectedChange = false;
+  }
+    if(selectedButton == SELECTED_WWID) 
+      genie.WriteObject(GENIE_OBJ_STRINGS,STRING_WWID_BUTTON_MASK,0); // bring highlighting box to the front for WWID
+    else if(selectedButton == SELECTED_DUR)
+      genie.WriteObject(GENIE_OBJ_STRINGS,STRING_DUR_BUTTON_MASK,0);  // bring highlighting box to the front for duration
+  // else if (selectedButton == SELECTED_NONE) //do nothing
+  genie.WriteStr(STRING_WWID,wwidBuf); // write wwid buffer to strings object
+  genie.WriteStr(STRING_DUR,durBuf); // write duration bufer to strings object 
+}
+/*void keyPress(int key)
 {
   Serial.print("Keypress received: ");
   Serial.println(key);
@@ -60,7 +111,7 @@ void keyPress(int key)
   }
   wwid[wwidIndex] = 0; // set the value at wwidIndex to be our null terminator
   genie.WriteStr(0,wwid); // write our char array to strings object 0
-}
+}*/
 void myGenieEventHandler (void)
 {
   int keyboardValue; // int to store keypress
@@ -78,8 +129,28 @@ void myGenieEventHandler (void)
     if (Event.reportObject.index == 0)	// If from Keyboard0
     {
       keyboardValue = genie.GetEventData(&Event); // Get data from Keyboard0
-      keyPress(keyboardValue); // pass data to the keyPress function
+      //keyPress(keyboardValue); // pass data to the keyPress function
     }
   }
+  else if(Event.reportObject.object == GENIE_OBJ_USERBUTTON)
+  {
+   if(Event.reportObject.index == BUTTON_WWID)
+    {
+      selectedButton = SELECTED_WWID;
+      selectedChange = true;
+    }
+   else if(Event.reportObject.index == BUTTON_DUR)
+   {
+     selectedButton = SELECTED_DUR;
+     selectedChange = true;
+   }
+   else if(Event.reportObject.index == BUTTON_TOOL)
+   {
+     selectedTool++;
+     if(selectedTool == TOOL_STRINGS_ELEMENTS) //went past last object
+       selectedTool = 0;
+   }
+  }
+  drawScreen();
 }
 
